09d8c35da5de50de9ca9f72ec8bb78f53063fefe
王二 <648764190@qq.com>
2020-04-01 23:05:03 +0800

    add grid

--- page/arknights/js/layout-grid.js
+++ page/arknights/js/layout-grid.js
@@ -8,19 +8,15 @@ window.Vue.component(LayoutGrid.name, LayoutGrid);
 function getLayoutGridTemplate() {
   return `
 <div class="layout-grid" @scroll.prevent>
-<div class="layout-grid-content">
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-  <div class="layout-grid-item">grid</div>
-</div>
+  <div class="layout-grid-content">
+    <div class="layout-grid-line"></div>
+    <div class="layout-grid-item" v-for="item in 20">
+      <div class="item-symbol"></div>
+      <div class="item-content">
+        input
+      </div>
+    </div>
+  </div>
 </div>
   `;
 }
